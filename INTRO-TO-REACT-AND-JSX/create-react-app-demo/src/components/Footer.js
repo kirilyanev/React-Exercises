5605e246e97149e8cc5f52bc1a1b0ff4ed94d549
Kiril Yanev <kl_yanev@abv.bg>
2023-07-29 23:38:00 +0300

    First React App update

---
+++ INTRO-TO-REACT-AND-JSX/create-react-app-demo/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>React App, All rigths reserved &copy;</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ INTRO-TO-REACT-AND-JSX/create-react-app-demo/src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    let headingMessage = 'Hello React World from custom component';
+
+    return (
+        <header>
+            <h1>{headingMessage}</h1>
+            <h2>The Best Framework Ever</h2>
+        </header>
+    );
+}
+
+export default Header;
