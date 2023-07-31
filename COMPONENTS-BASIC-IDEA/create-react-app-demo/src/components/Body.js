4f7c4dcd3017d20b8cc27cab21509e8ced2c4a7e
Kiril Yanev <kl_yanev@abv.bg>
2023-07-31 20:49:04 +0300

    Create Body.js component

---
+++ COMPONENTS-BASIC-IDEA/create-react-app-demo/src/components/Body.js
@@ -0,0 +1,21 @@
+// Deafult import
+
+// import React from 'react';
+
+// class Body extends React.Component {
+
+// }
+
+import { Component } from 'react';
+
+class Body extends Component {
+    render() {
+        return (
+            <main>
+                <p>Lorem ipsum dolor</p>
+            </main>
+        );
+    }
+}
+
+export default Body;
