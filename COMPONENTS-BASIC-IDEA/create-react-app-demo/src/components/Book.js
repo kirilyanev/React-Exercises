e82cb193cffdab5fed2da200e01d7001d8a1c540
Kiril Yanev <kl_yanev@abv.bg>
2023-08-04 18:07:06 +0300

    Update Book.js
    Added onClick event in functional component

--- COMPONENTS-BASIC-IDEA/create-react-app-demo/src/components/Book.js
+++ COMPONENTS-BASIC-IDEA/create-react-app-demo/src/components/Book.js
@@ -1,6 +1,6 @@
 function Book(props) {
     return (
-        <article>
+        <article onClick={() => console.log(`${props.title} is clicked!`)}>
             <h3>{props.title}</h3>
             <p>{props.description}</p>
         </article>
