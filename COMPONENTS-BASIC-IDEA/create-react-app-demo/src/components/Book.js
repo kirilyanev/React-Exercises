8f419ecf09adf251de20f5092ac4ce4bfa14c7c9
Kiril Yanev <kl_yanev@abv.bg>
2023-08-11 22:07:28 +0300

    Update Book.js

--- COMPONENTS-BASIC-IDEA/create-react-app-demo/src/components/Book.js
+++ COMPONENTS-BASIC-IDEA/create-react-app-demo/src/components/Book.js
@@ -1,8 +1,14 @@
+import style from './Book.module.css';
+
 function Book(props) {
+
+    const author = props.author ? <strong>{props.author}</strong> : <strong>Pesho</strong>;
+
     return (
-        <article>
+        <article className={style.article}>
             <h3 onClick={props.clickHandler}>{props.title}</h3>
-            <p>{props.description || 'Default Description'}</p>
+            <span className={style.description}>{props.description || 'Default Description'}</span>
+            <p className={style.author}>{author}</p>
         </article>
     );
 }
