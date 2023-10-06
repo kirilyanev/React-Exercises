f068a55ca915b608652f30501e0d4ca326e233d8
Kiril Yanev <kl_yanev@abv.bg>
2023-10-06 17:15:21 +0300

    feat: add 'Categories' component

--- REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/App.js
+++ REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/App.js
@@ -1,13 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
+import Categories from './components/Categories/Categories.js';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <Header />
-
-      <h1>Hello</h1>
+      <Routes>
+        <Route path='/' Component={Categories} />
+      </Routes>
 
 
       <Footer />
