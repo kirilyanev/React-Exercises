4ef2a558256bed0eec93d1aa030d3bd5c2acf75c
Kiril Yanev <kl_yanev@abv.bg>
2023-10-07 14:00:59 +0300

    feat: create 'Pet' component

--- REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/Categories/Categories.js
+++ REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/Categories/Categories.js
@@ -1,56 +1,38 @@
+import { Component } from "react";
+import Pet from "../Pet/Pet.js";
 import CategoryNavigation from "./CategoryNavigation/CategoryNavigation.js";
 
-import { useParams} from 'react-router-dom';
+class Categories extends Component {
+    constructor(props) {
+        super(props);
 
-const Categories = () => {
-    const params = useParams();
-    console.log(params);
+        this.state = {
+            pets: []
+        };
+    }
 
-    return (
-        <section className="dashboard">
-            <h1>Dashboard</h1>
+    componentDidMount() {
+        fetch('http://localhost:5000/pets')
+        .then(res => res.json())
+        .then(res => this.setState({pets: res}))
+        .catch(error => console.log(error));
+    }
 
-            <CategoryNavigation />
+    render() {
+        console.log(this.state.pets);
 
-            <ul className="other-pets-list">
-                <li className="otherPet">
-                    <h3>Name: Gosho</h3>
-                    <p>Category: Cat</p>
-                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png" /></p>
-                    <p className="description">This is not my cat Gosho</p>
-                    <div className="pet-info">
-                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
-                        <a href="#"><button className="button">Details</button></a>
-                        <i className="fas fa-heart"></i> <span> 2</span>
-                    </div>
-                </li>
-                <li className="otherPet">
-                    <h3>Name: Gosho</h3>
-                    <p>Category: Cat</p>
-                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png" /></p>
-                    <p className="description">This is not my cat Gosho</p>
-                    <div className="pet-info">
-                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
-                        <a href="#"><button className="button">Details</button></a>
-                        <i className="fas fa-heart"></i> <span> 2</span>
-                    </div>
+        return (
+            <section className="dashboard">
+                <h1>Dashboard</h1>
 
-                </li>
-                <li className="otherPet">
-                    <h3>Name: Kiro</h3>
-                    <p>Category: Dog</p>
-                    <p className="img"><img src="http://www.stickpng.com/assets/images/580b57fbd9996e24bc43bbde.png" />
-                    </p>
-                    <p className="description">This is my dog Kiro</p>
-                    <div claclassName="pet-info">
-                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
-                        <a href="#"><button className="button">Details</button></a>
-                        <i className="fas fa-heart"></i> <span> 4</span>
-                    </div>
-                </li>
-            </ul>
-        </section>
-    );
+                <CategoryNavigation />
+
+                <ul className="other-pets-list">
+
+                </ul>
+            </section>
+        );
+    }
 };
 
 export default Categories;
--- REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/Categories/CategoryNavigation/CategoryNavigation.js
+++ REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/Categories/CategoryNavigation/CategoryNavigation.js
@@ -4,16 +4,16 @@ const CategoryNavigation = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li><NavLink className={({ isActive }) => isActive ? "nav-link-selected" : ""} to="/categories/all">All</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? "nav-link-selected" : ""} to="/categories/cats">Cats</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? "nav-link-selected" : ""} to="/categories/dogs">Dogs</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? "nav-link-selected" : ""} to="/categories/parrots">Parrots</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? "nav-link-selected" : ""} to="/categories/reptiles">Reptiles</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? "nav-link-selected" : ""} to="/categories/other">Other</NavLink></li>
+                <li><NavLink to="/categories/all">All</NavLink></li>
+                <li><NavLink to="/categories/cats">Cats</NavLink></li>
+                <li><NavLink to="/categories/dogs">Dogs</NavLink></li>
+                <li><NavLink to="/categories/parrots">Parrots</NavLink></li>
+                <li><NavLink to="/categories/reptiles">Reptiles</NavLink></li>
+                <li><NavLink to="/categories/other">Other</NavLink></li>
             </ul>
 
             <style jsx>{`
-                .nav-link-selected {
+                .active {
                     background-color: lightgreen !important;
                 }    
             `}</style>
