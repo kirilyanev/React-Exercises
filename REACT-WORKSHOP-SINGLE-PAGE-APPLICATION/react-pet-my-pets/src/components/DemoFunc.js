edc5e4597518a440af7df410eefd6189184167ed
Kiril Yanev <kl_yanev@abv.bg>
2023-10-19 19:54:52 +0300

    useState, useEffect hooks demo

--- REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/DemoFunc.js
+++ REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/DemoFunc.js
@@ -1,16 +1,54 @@
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 const DemoFunc = () => {
+    // const [state, setState] = useState({
+    //     count: 11,
+    //     step: 1,
+    // });
+
+    const [count, setCount] = useState(1);
+    const [step, setStep] = useState(1);
+    const [character, setCharacter] = useState({name: 'nobody'});
+
+    useEffect(() => {
+        fetch(`http://swapi.dev/api/people/${step}`)
+            .then(res => res.json())
+            .then(result => setCharacter(result));
+
+        return () => {
+            console.log('ComponentWillUnmount');
+        };
+    }, [step]);
 
     const onCounterButtonClickHandler = () => {
-        console.log('increase');
+        // setState(oldState => ({...oldState, count: oldState.count + oldState.step}));
+        setCount(oldState => oldState + step);
     };
 
+    const onStepSelectChangeHandler = (e) => {
+        const stepValue = Number(e.target.value);
+
+        // setState(oldState => ({...oldState, step: stepValue}));
+        setStep(stepValue);
+    };
 
     return (
         <div>
-            <h1>Counter</h1>
-            <div>0</div>
+            <h1>{character.name}'s Counter</h1>
+            {/* <div>{state.count}</div> */}
+            <div>{count}</div>
             <button onClick={onCounterButtonClickHandler}>Increment</button>
+            <label htmlFor="step">Step</label>
+            <select name="step" id="step" onChange={onStepSelectChangeHandler}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
         </div>
     );
 };
