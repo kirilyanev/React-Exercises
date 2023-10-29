5ff78190e96fe3b444c0c56038eb1801ea175cc9
Kiril Yanev <kl_yanev@abv.bg>
2023-10-29 21:12:53 +0200

    Add mocking

--- REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/PetCard/PetCard.test.js
+++ REACT-WORKSHOP-SINGLE-PAGE-APPLICATION/react-pet-my-pets/src/components/PetCard/PetCard.test.js
@@ -1,10 +1,13 @@
 // import { ReactDOM } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen, fireEvent, waitFor} from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import React from 'react';
 
 import PetCard from './PetCard.js';
+import * as petsService from '../../services/petsService.js';
+
+jest.mock('../../services/petsService.js');
 
 describe('PetCard Component', () => {
     it('Should display name', () => {
@@ -17,17 +20,22 @@ describe('PetCard Component', () => {
     });
 
     it('Should increase likes when pet button is pressed', async () => {
+        petsService.pet.mockResolvedValue({ likes: 6 });
+
         render(
             <BrowserRouter>
-                <PetCard likes={5}/>
+                <PetCard likes={5} />
             </BrowserRouter>
         );
 
-        // fireEvent.click(screen.getByText('Pet'));
+        // Click the 'Pet' button
+        fireEvent.click(screen.getByText('Pet'));
+        
+        // Wait for state to update
+        await waitFor(() => screen.getByText('6'));
 
-        // await waitFor(() => screen.getByText('Pet'));
-
-        expect(document.querySelector('.pet-info span').textContent).toBe('5');
+        expect(document.querySelector('.pet-info span').textContent).toBe('6');
+        // expect(screen.getByText('6')).toBeInTheDocument();
     });
 
 
